0eb3abb5138eb5481ae9cba4bab94e33b3b0b4c2
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2021-11-25 00:01:07 +0300

    Added Works and Partial Contact Page

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({ ease: "none", duration: 2 });
+
+const t1 = gsap.timeline();
+
+t1.from(".works-page-content-two", { xPercent: -100 })
+  .from(".works-page-content-three", { yPercent: 100 })
+  .from(".works-page-content-four", { xPercent: -100 })
+  .from(".works-page-content-five", { xPercent: -100 });
+
+ScrollTrigger.create({
+  animation: t1,
+  trigger: ".works-page-container",
+  start: "top top",
+  end: "+=4000",
+  scrub: true,
+  pin: true,
+  anticipatePin: 1,
+});
